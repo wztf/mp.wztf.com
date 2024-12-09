80a6daeb58e665b33dee0376777505a52d285df5
cleveng <cleveng@gmail.com>
2024-12-09 19:56:24 +0800

    fix: --wip--

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -2,7 +2,68 @@
 const Page = () => {
   return (
     <>
-      <h1>privacy</h1>
+      <div className='bg-white text-black dark:bg-black dark:text-white'>
+        <div className='container mx-auto px-4 py-8'>
+          <p className='mb-4'>Effective Date: 2024-7-15</p>
+          <div className='w-full flex'>
+            <h1 className='text-3xl font-bold mb-4 text-orange-800'>Terms of Service for Your Business </h1>
+          </div>
+          <br />
+          <p className='mb-4 text-base'>
+            Welcome to the website. Please read these terms of service carefully before using the website. By using the
+            website, you agree to be bound by these terms of service. If you do not agree to these terms of service, you
+            may not use the website. These terms of service govern your use of the website and all services provided by
+            the website. If you do not agree to these terms of service, you may not use the website.
+          </p>
+          <br />
+          <div className='pb-5'>
+            <h2 className='font-bold text-orange-700 dark:text-orange-300'>General</h2>
+            <br />
+            <ul>
+              <li className='list-disc'>
+                <p className='text-base'>
+                  By accessing this website, you agree to be bound by these terms of service, all applicable laws and
+                  regulations, and agree that you are responsible for compliance with any applicable local laws. If you
+                  do not agree with any of these terms, you are prohibited from using or accessing this site. The
+                  materials contained in this website are protected by applicable copyright and trademark law.
+                </p>
+              </li>
+              <br />
+              <li className='list-disc'>
+                <p className='text-base'>
+                  We reserve the right to change these terms of service at any time without notice. By using this
+                  website you are agreeing to be bound by the then current version of these terms of service. Any
+                  updates you will be notified via email.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <pre>
+        隐私政策
+        本隐私政策（以下简称“本政策”）旨在帮助您了解我们如何收集、使用、存储及保护您的个人信息。请在使用我们的服务前，仔细阅读并理解本政策。如果您使用我们的服务，表示您同意我们按照本政策的内容收集、使用、存储及共享您的个人信息。
+        1. 我们收集的信息 在您使用我们的服务时，我们可能会收集以下类型的信息：
+        个人身份信息：包括您的姓名、邮箱地址、手机号码、身份证号码等；
+        设备信息：包括您设备的型号、操作系统版本、设备识别码、IP地址、网络状态等；
+        服务使用数据：包括您的操作日志、使用频率、访问页面、点击数据等。 2. 信息的使用 我们收集的信息将用于以下目的：
+        提供、维护、改进我们的服务； 向您发送与服务相关的通知和信息； 分析和研究用户行为，以优化用户体验；
+        法律要求或保护您的权益。 3. 信息的存储与保护
+        我们会采取合理的技术和管理措施，保障您的个人信息安全。您的信息将存储在我们的服务器中，并遵循严格的安全控制标准。尽管我们会尽最大努力保护您的信息，但无法保证绝对的安全性，因此请您谨慎使用。
+        4. 信息共享
+        除非根据法律法规或您的授权，我们不会将您的个人信息出售、出租或以其他方式向第三方披露。然而，在以下情况下，我们可能会共享您的信息：
+        与我们的合作伙伴或服务提供商共享，为您提供服务； 根据法律要求、政府要求或司法程序提供您的信息；
+        为了保护我们的合法权益、财产和用户的安全。 5. 第三方链接与服务
+        我们的服务中可能包含指向第三方网站或服务的链接。我们不控制这些网站和服务的隐私政策，因此我们建议您在访问第三方网站或服务时，阅读其隐私政策。我们对第三方网站的内容和隐私实践不承担任何责任。
+        6. 您的权利 根据法律规定，您对您的个人信息拥有以下权利： 查询、访问、更新您的个人信息；
+        请求删除您的个人信息（在符合条件的情况下）； 撤回对个人信息处理的同意。
+        如果您希望行使这些权利，您可以通过联系我们的方式进行。 7. 未成年人隐私保护
+        我们非常重视未成年人的隐私保护。如果您未满18岁，请在父母或法定监护人的同意下使用我们的服务。我们不会故意收集未成年人的个人信息。
+        8. 本政策的更新
+        我们可能会根据法律、法规的变化或业务发展的需要更新本政策。任何修改后的隐私政策将在本页面公布，且自公布之日起生效。请定期查看本政策，以了解我们的隐私保护措施是否有更新。
+        9. 联系我们 如果您对本隐私政策有任何疑问或需要进一步了解您的个人信息，请通过以下方式联系我们：
+        联系邮箱：[您的邮箱地址] 联系电话：[您的联系电话]
+      </pre>
       <div className='flex items-center flex-col justify-center lg:flex-row py-28 px-6 md:px-24 md:py-20 lg:py-32 gap-16 lg:gap-28'>
         <div className='w-full lg:w-1/2'>
           <img className='hidden lg:block' src='https://i.ibb.co/v30JLYr/Group-192-2.png' alt='' />
