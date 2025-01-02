13106a960cc82c05582a7643952a8b24ac167c75
cleveng <cleveng@gmail.com>
2025-01-02 22:36:04 +0800

    perf: update middleware

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,20 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value
-  if (!token) {
-    return NextResponse.redirect(new URL('/signin', request.url))
+  try {
+    const token = request.cookies.get('token')?.value
+    if (!token) {
+      return NextResponse.redirect(new URL('/signin', request.url))
+    }
+
+    // TODO: 处理用户未激活 || 用户菜单等
+    // https://blog.openreplay.com/how-to--authentication-middleware-in-nextjs/
+
+    return NextResponse.next()
+  } catch (error) {
+    console.error('Middleware Error:', error)
+    return new NextResponse('Internal Server Error', { status: 500 })
   }
-
-  // TODO: 处理用户未激活 || 用户菜单等
-  // https://blog.openreplay.com/how-to--authentication-middleware-in-nextjs/
-
-  return NextResponse.next()
 }
 
 export const config = {
